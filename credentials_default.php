5c2dea347c97074cfcd6df8c61b903ffc13939d9
Peter Korduan <peter.korduan@gdi-service.de>
2025-05-19 16:08:31 +0200

    credentials_default anlegen.

---
+++ credentials_default.php
@@ -0,0 +1,7 @@
+<?
+  define('POSTGRES_HOST', 'pgsql');
+  define('POSTGRES_USER', 'kvwmap');
+  define('POSTGRES_PASSWORD', 'secret');
+  define('POSTGRES_DBNAME', 'kvwmapsp');
+  define('POSTGRES_HOSTS_ALLOWED', '172.17.%');
+?>
